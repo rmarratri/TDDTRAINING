bd9f19ae751378aa1ebb1904d904823e2d5e9064
Ryan Marr <>
2022-01-06 14:45:39 -0800

    feat(training): Chapter 2
    Competed Chapter 2
    
    [ICTRAINING-895]

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,28 @@
 from selenium import webdriver
+import unittest
 import geckodriver_autoinstaller
 
 geckodriver_autoinstaller.install()
 MAX_WAIT = 10
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):  
 
-print(browser.title)
-assert 'The install worked successfully! Congratulations!' in browser.title
+    def setUp(self):  
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):  
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):  
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)  
+        self.fail('Finish the test!')  
+
+        # She is invited to enter a to-do item straight away
+
+if __name__ == '__main__':  
+    unittest.main()
