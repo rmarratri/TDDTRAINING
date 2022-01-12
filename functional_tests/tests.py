d2bad3a6685ac9fd4bf0274f2a7f40e96104e859
Ryan Marr <>
2022-01-12 12:30:21 -0800

    feat(training): changes to run in travis ci
    running in server
    
    [ICTRAINING-894]

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -74,7 +74,6 @@ class NewVisitorTest(unittest.TestCase):
         # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
